3600242d6f31b362bb28b42c2245f04485acfcdb
Krutik Aghera <agherakrutik99@gmail.com>
2023-08-22 23:47:03 +0530

    trying with Mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import LoginPage from "./pages/LoginPage"
+import { Box } from '@mui/material'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      
-    </div>
+    <Box
+      sx={{
+        backgroundColor: 'primary.dark',
+      }}
+    >
+      hiiiiiiii
+      <LoginPage />
+    </Box>
   )
 }
 
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,8 +1,8 @@
 
 export default function LoginPage(){
     return (
-        <div>
-            
+        <div className="loginPage">
+            hiii
         </div>
     )
 }
