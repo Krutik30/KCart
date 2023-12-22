29bfe4054e42ef2c18cc421181bca4b4b483c63f
Krutik30 <agherakrutik99@gmail.com>
2023-12-22 21:08:57 +0530

    add service and connect to app

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { UserService } from './user.service';
 
-@Module({})
-export class UserModule {}
+@Module({
+  providers: [UserService]
+})
+export class UserModule { }
