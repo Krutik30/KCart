f3c5100d4f05ea708b2b1dfb33870a5b8983a283
Krutik30 <agherakrutik99@gmail.com>
2023-12-24 21:14:19 +0530

    Revert "add service and connect to app"
    This reverts commit 29bfe4054e42ef2c18cc421181bca4b4b483c63f.

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,7 +1,4 @@
 import { Module } from '@nestjs/common';
-import { UserService } from './user.service';
 
-@Module({
-  providers: [UserService]
-})
-export class UserModule { }
+@Module({})
+export class UserModule {}
