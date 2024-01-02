0029c5a5e071683588a7f5718935798e0d5f910a
Krutik30 <agherakrutik99@gmail.com>
2024-01-02 15:51:24 +0530

    adding auth controller

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,4 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private authService: AuthService) { }
+
+    // post request of /auth/signUp route
+    @Post('signUp')
+    signUp() {
+        return this.authService.signUp();
+    }
+
+    @HttpCode(HttpStatus.OK)
+    @Post('signIn')
+    signIn() {
+        return this.authService.signIn();
+    }
+
+}
